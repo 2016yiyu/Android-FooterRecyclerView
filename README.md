473ec7bcea2ac45c35009647219b63ed6daa883c
e <512501381@qq.com>
2016-11-27 19:13:06 +0800

    commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Android-FooterRecyclerView
-通过自定义RecyclerView实现上拉加载功能
+通过自定义RecyclerView实现上拉加载功能,有时间将会把下拉刷新添加进去
 
 ##使用配置<br>
 _ _ _ _
@@ -29,18 +29,15 @@ dependencies {
 ```
 ##方法说明
 ```
- footer_rv=(FooterRecyclerView)findViewById(R.id.footer_rv);
-        //允许滑动到底部上拉加载
-        footer_rv.setEnabled(true);
-        //设置滑到底部自动加载
-        footer_rv.setmIsAutoLoadMore(true);
+        footer_rv.setEnabled(true); //允许滑动到底部上拉加载
+        footer_rv.setmIsAutoLoadMore(true);   //设置滑到底部自动加载
         //设置监听默认开启上拉加载
         footer_rv.setOnLoadListener(new FooterRecyclerView.OnLoadMoreListener() {
             @Override
             public void onLoadMore() {
-                footer_rv.onLoadMoreComplete();//加载完成
             }
         });
+          footer_rv.onLoadMoreComplete();//加载完成
  ```
 #####感谢
  添加头部尾部借鉴:http://blog.csdn.net/jxxfzgy/article/details/47012097
--- app/src/main/java/recyclerview/demo/MainUI.java
+++ app/src/main/java/recyclerview/demo/MainUI.java
@@ -1,10 +1,14 @@
 package recyclerview.demo;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.MainAdapter;
 import footer.android.FooterRecyclerView;
 
 /**
@@ -13,25 +17,53 @@ import footer.android.FooterRecyclerView;
 
 public class MainUI extends AppCompatActivity {
     private FooterRecyclerView footer_rv;
+    private MainAdapter m_adapter;
+    int i;
+    private List<String> list=new ArrayList<String>();
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
        setContentView(R.layout.main_ui);
-        initView();
+        setData();
+       initView();
     }
 
+    private void setData() {
+        for (int i=0;i<8;i++){
+            list.add(""+i);
+        }
+    }
+public  void addData(){
+    i++;
+    for (int i=0;i<3;i++){
+        list.add(""+i);
+    }
+    m_adapter.setList(list);
+    m_adapter.notifyDataSetChanged();
+    footer_rv.onLoadMoreComplete();
+}
     private void initView() {
+        m_adapter=new MainAdapter(this,list);
         footer_rv=(FooterRecyclerView)findViewById(R.id.footer_rv);
-        //允许滑动到底部上拉加载
-        footer_rv.setEnabled(true);
+        footer_rv.setLayoutManager(new LinearLayoutManager(this));
         //设置滑到底部自动加载
-        footer_rv.setmIsAutoLoadMore(true);
+        footer_rv.setmIsAutoLoadMore(false);
         //设置监听默认开启上拉加载
         footer_rv.setOnLoadListener(new FooterRecyclerView.OnLoadMoreListener() {
             @Override
             public void onLoadMore() {
-                footer_rv.onLoadMoreComplete();//加载完成
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (i<2){
+                            addData();
+                        }else {
+                            footer_rv.closeFooter("大哥，没有更多数据了");
+                        }
+                    }
+                },2000);
             }
         });
+        footer_rv.setAdapter(m_adapter);
     }
 }
